ed3976bb2a7362868dcf5923dc8a85e23b51782b
Arunabh Gupta <arunabh.gupta.min@itbhu.ac.in>
2023-11-24 22:01:03 +0530

    fixed a small bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import './App.css'
-import './components/ImageDisplay'
 import InputForm from './components/InputForm/InputForm'
 import Header from './components/Header/Header'
-
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 function App() {
   
   return (
